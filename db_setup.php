2fc15f34b9f65db62994d3f3030318c03ed63286
ryupatterson <54785861+ryupatterson@users.noreply.github.com>
2021-10-26 03:03:09 -0400

    db setup
    added hanja/kanji databases and setup code to db_setup

--- db_setup.php
+++ db_setup.php
@@ -2,7 +2,7 @@
     include('./database_connection.php');
     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
     $db = new mysqli($dbserver, $dbuser, $dbpass, $dbdatabase);
-    
+
     /*
      *$db->query("drop table if exists question;");
      *$db->query("create table question (
@@ -20,4 +20,56 @@
         username text not null,
         password text not null,
         primary key (id));");
+
+
+    $db->query("drop table if exists kanji;");
+    $db->query("create table kanji(
+      id int not null auto_increment,
+      literal text not null,
+      on_yomi text not null,
+      kun_yomi text not null,
+      meaning text not null,
+      stroke_count int not null,
+      primary key (id)
+    );");
+
+    $db->query("drop table if exists hanja");
+    $db->query("create table hanja(
+      id int not null auto_increment,
+      literal text not null,
+      sound text not null,
+      meaning text not null,
+      primary key (id)
+    );");
+
+    $data = json_decode(file_get_contents("db_files/kanji.txt"),true);
+    $hanja = json_decode(file_get_contents("db_files/hanja.json"),true);
+
+    // Kanji set-up from 'kanji.txt'.
+    $stmt = $db->prepare("insert into kanji (literal, on_yomi, kun_yomi, meaning, stroke_count) values (?,?,?,?,?);");
+    foreach ($data['character'] as $x) {
+      $stmt->bind_param('ssssi', $x['literal'],$x['on_yomi'],$x["kun_yomi"],$x['meaning'],$x['stroke_count']);
+      if(!$stmt->execute()){
+        echo 'Could not add kanji';
+      }
+    }
+
+    // Hanja table data insertion from hanja.json
+    $f = $db->prepare("insert into hanja (literal, sound, meaning) values (?,?,?);");
+    for($i = 0; $i <= 7948;$i++  ){
+      $sounds = "";
+      $meanings = "";
+      foreach ($hanja[$i]['sounds'] as $x) {
+        $sounds = $sounds . $x . ', ';
+      }
+      foreach ($hanja[$i]['meanings'] as $x) {
+        $meanings = $meanings . $x . ', ';
+      }
+      $sounds = trim($sounds, ', ');
+      $meanings = trim($meanings, ', ');
+      $f->bind_param("sss", $hanja[$i]['hanja'], $sounds, $meanings);
+      if(!$f->execute()){
+        echo 'could not add';
+      }
+    }
 ?>
